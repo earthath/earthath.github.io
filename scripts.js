3c9acc4c4c2bd030130831ac6eb46f9086a673f2
earthath <earthathip22@gmail.com>
2025-01-03 18:01:44 +0900

    Test commit3

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
-// scripts.js
-
 // Typing Effect
 document.addEventListener('DOMContentLoaded', () => {
     const nameElement = document.getElementById('typing-name');
-    const nameText = nameElement.textContent;
-    nameElement.textContent = '';
+    const nameText = nameElement.innerHTML;
+    nameElement.innerHTML = '';
     let i = 0;
 
     function typeWriter() {
         if (i < nameText.length) {
-            nameElement.textContent += nameText.charAt(i);
+            nameElement.innerHTML += nameText.charAt(i);
             i++;
             setTimeout(typeWriter, 100);
         }
@@ -35,4 +33,38 @@ document.querySelectorAll('nav a').forEach(anchor => {
         e.preventDefault();
         gsap.to(window, {duration: 2, scrollTo: {y: this.getAttribute('href'), offsetY: 50}});
     });
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('resume-kr').addEventListener('click', () => {
+//         const userChoice = confirm(
+//             'Unfortunately, you don\'t have access to the file. Contact me for more information.\n\n불행히도 파일에 접근할 수 없습니다. 자세한 내용은 저에게 문의하십시오.\n\nContact me?'
+//         );
+//         if (userChoice) {
+//             document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+//         }
+//     });
+
+//     document.getElementById('resume-en').addEventListener('click', () => {
+//         const userChoice = confirm(
+//             'Unfortunately, you don\'t have access to the file. Contact me for more information.\n\n불행히도 파일에 접근할 수 없습니다. 자세한 내용은 저에게 문의하십시오.\n\nContact me?'
+//         );
+//         if (userChoice) {
+//             document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+//         }
+//     });
+// });
+// Resume Buttons
+document.getElementById('resume-kr').addEventListener('click', () => {
+    const userChoice = confirm('Unfortunately, you don\'t have access to the file. Contact me for more information.\n\n불행히도 파일에 접근할 수 없습니다. 자세한 내용은 저에게 문의하십시오.\n\nContact me?');
+    if (userChoice) {
+        window.location.href = '#contact';
+    }
+});
+
+document.getElementById('resume-en').addEventListener('click', () => {
+    const userChoice = confirm('Unfortunately, you don\'t have access to the file. Contact me for more information.\n\n불행히도 파일에 접근할 수 없습니다. 자세한 내용은 저에게 문의하십시오.\n\nContact me?');
+    if (userChoice) {
+        window.location.href = '#contact';
+    }
 });
